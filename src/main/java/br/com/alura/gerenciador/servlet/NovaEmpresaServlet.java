8a17504b4c2db48bf46c68e80b04a800f5a19cef
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-11-08 13:11:50 -0300

    recebedo parametro no '/novaEmpresa'

---
+++ src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,18 @@
+package br.com.alura.gerenciador.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class NovaEmpresaServlet extends HttpServlet {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Cadastrando nova empresa");
+		String nomeEmpresa = request.getParameter("nome");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>Empresa " + nomeEmpresa +" cadastrada com sucesso!</body></html>");
+	}
+}
